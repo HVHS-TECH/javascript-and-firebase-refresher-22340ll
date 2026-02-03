91238492f7543b3a5bf32de6743913eab734cf59
22340ll <22340ll@hvhs.school.nz>
2026-02-04 12:23:49 +1300

    test test 123

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-var messageSpace = getElementbyId("welcomeMessage");
+document.addEventListener("DOMContentLoaded", function() {
+var messageSpace = document.getElementById("welcomeMessage");
 messageSpace.innerHTML = "Your not in the right place! get out";
+console.log("raaaahhhhh")
+});
+// 3. Define the function to run on click
+function changeText() {
+    const paragraph = document.getElementById('demo');
+    paragraph.textContent = 'The button was clicked!';
+}
+
+// 4. Select the button and add an event listener
+const button = document.getElementById('myButton');
+button.addEventListener('click', changeText);
