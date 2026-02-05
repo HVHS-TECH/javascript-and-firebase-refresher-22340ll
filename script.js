4b568cdba7a7d52df5cda9ca43a74bc8dcc8938f
22340ll <22340ll@hvhs.school.nz>
2026-02-05 13:27:41 +1300

    what a mario world

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
-var messageSpace = document.getElementById("welcomeMessage");
-messageSpace.innerHTML = "Your not in the right place! get out";
-console.log("raaaahhhhh")
+    var messageSpace = document.getElementById("welcomeMessage");
+    messageSpace.innerHTML = "Your not in the right place! get out";
+    console.log("raaaahhhhh")
 });
 //Javascript
 // Import the functions you need from the SDKs you need
@@ -10,18 +10,18 @@ console.log("raaaahhhhh")
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    const firebaseConfig = {
-    apiKey: "AIzaSyB_CnBPy37yHOG3tgT9IHAKsyx3KMOKXfU",
-    authDomain: "uuuuhhhhhh-34d89.firebaseapp.com",
-    databaseURL: "https://uuuuhhhhhh-34d89-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "uuuuhhhhhh-34d89",
-    storageBucket: "uuuuhhhhhh-34d89.firebasestorage.app",
-    messagingSenderId: "198705171993",
-    appId: "1:198705171993:web:6b0029a02834c9fb59e488",
-    measurementId: "G-2KSDSKRWG7"
-    };
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+  apiKey: "AIzaSyB_CnBPy37yHOG3tgT9IHAKsyx3KMOKXfU",
+  authDomain: "uuuuhhhhhh-34d89.firebaseapp.com",
+  databaseURL: "https://uuuuhhhhhh-34d89-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "uuuuhhhhhh-34d89",
+  storageBucket: "uuuuhhhhhh-34d89.firebasestorage.app",
+  messagingSenderId: "198705171993",
+  appId: "1:198705171993:web:6b0029a02834c9fb59e488",
+  measurementId: "G-2KSDSKRWG7"
+  };
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
@@ -34,6 +34,6 @@ function changeText() {
 }
 
 // 4. Select the button and add an event listener
-const button = document.getElementById('myButton');
-button.addEventListener('click', changeText);
+    const button = document.getElementById('myButton');
+    button.addEventListener('click', changeText);
 
